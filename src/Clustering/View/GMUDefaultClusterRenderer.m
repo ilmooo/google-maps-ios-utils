33364ac240a75fb4a40fde0131914d35e0d8e569
ilmooo <9102335+ilmooo@users.noreply.github.com>
2024-05-31 12:21:39 +0300

    Change minimum cluster size and maximum cluster zoom

--- src/Clustering/View/GMUDefaultClusterRenderer.m
+++ src/Clustering/View/GMUDefaultClusterRenderer.m
@@ -24,11 +24,11 @@
 #import "GMUWrappingDictionaryKey.h"
 
 // Clusters smaller than this threshold will be expanded.
-static const NSUInteger kGMUMinClusterSize = 4;
+static const NSUInteger kGMUMinClusterSize = 2;
 
 // At zooms above this level, clusters will be expanded.
 // This is to prevent cases where items are so close to each other than they are always grouped.
-static const float kGMUMaxClusterZoom = 20;
+static const float kGMUMaxClusterZoom = 21;
 
 // Animation duration for marker splitting/merging effects.
 static const double kGMUAnimationDuration = 0.5;  // seconds.
